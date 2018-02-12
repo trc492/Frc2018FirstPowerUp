8a8051b910aa9dc03f92422d8e6b4e7238281b78
_c0da_ <duvictor514@gmail.com>
2018-02-11 20:13:18 -0800

    Fixed formatting in SpeakStandClearWhenEnabledTask class.

--- src/team492/SpeakStandClearWhenEnabledTask.java
+++ src/team492/SpeakStandClearWhenEnabledTask.java
@@ -6,7 +6,8 @@ import trclib.TrcTaskMgr;
 import trclib.TrcTaskMgr.TaskType;
 import trclib.TrcUtil;
 
-public class SpeakStandClearWhenEnabledTask implements TrcTaskMgr.Task {
+public class SpeakStandClearWhenEnabledTask implements TrcTaskMgr.Task 
+{
 	
 	private static final double SPEAK_PERIOD_SECONDS = 10.0; // Speaks once every this # of seconds.
 	private static final String TASK_NAME = "SpeakStandClearWhenEnabledTask";
@@ -14,7 +15,8 @@ public class SpeakStandClearWhenEnabledTask implements TrcTaskMgr.Task {
 	private final FrcEmic2TextToSpeech tts;
 	private double nextTimeToSpeakSeconds = 0; // 0 means disabled, no need to speak
 	
-	public SpeakStandClearWhenEnabledTask(FrcEmic2TextToSpeech tts) {
+	public SpeakStandClearWhenEnabledTask(FrcEmic2TextToSpeech tts)
+	{
 		this.tts = tts;
 		
 		TrcTaskMgr taskMgr = TrcTaskMgr.getInstance();
@@ -23,43 +25,53 @@ public class SpeakStandClearWhenEnabledTask implements TrcTaskMgr.Task {
 	}
 		
 	@Override
-	public void startTask(RunMode runMode) {
-		if (runMode != RunMode.DISABLED_MODE && runMode != RunMode.INVALID_MODE) {
+	public void startTask(RunMode runMode) 
+	{
+		if (runMode != RunMode.DISABLED_MODE && runMode != RunMode.INVALID_MODE) 
+		{
 			// Robot is unsafe
 			nextTimeToSpeakSeconds = TrcUtil.getCurrentTime();
-		} else {
+		} 
+		else
+		{
 			// Robot is safe
 			nextTimeToSpeakSeconds = 0;
 		}
 	}
 
 	@Override
-	public void postPeriodicTask(RunMode runMode) {
+	public void postPeriodicTask(RunMode runMode) 
+	{
 		double currentTime = TrcUtil.getCurrentTime();
-		if (nextTimeToSpeakSeconds > 0 && currentTime >= nextTimeToSpeakSeconds) {
+		if (nextTimeToSpeakSeconds > 0 && currentTime >= nextTimeToSpeakSeconds) 
+		{
 			tts.speak("stand clear");
 			nextTimeToSpeakSeconds = currentTime + SPEAK_PERIOD_SECONDS; 
 		}
 	}
 
 	@Override
-	public void stopTask(RunMode runMode) {
-		// Not used.
+	public void stopTask(RunMode runMode) 
+	{		// Not used. 
+	    
 	}
 
 	@Override
-	public void prePeriodicTask(RunMode runMode) {
+	public void prePeriodicTask(RunMode runMode)
+	{
 		// Not used.
 	}
 
 	
 	@Override
-	public void preContinuousTask(RunMode runMode) {
+	public void preContinuousTask(RunMode runMode)
+	{
 		// Not used.
 	}
 
 	@Override
-	public void postContinuousTask(RunMode runMode) {
+	public void postContinuousTask(RunMode runMode) 
+	{
 		// Not used.
 	}
 }
