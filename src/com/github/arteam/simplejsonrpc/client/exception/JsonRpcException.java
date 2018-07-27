480f0a65669f4fab6ca0524d35dbb9b633b3aecd
_c0da_ <duvictor514@gmail.com>
2018-07-27 12:16:00 -0700

    Added JSON-RPC libraries.

---
+++ src/com/github/arteam/simplejsonrpc/client/exception/JsonRpcException.java
@@ -0,0 +1,30 @@
+package com.github.arteam.simplejsonrpc.client.exception;
+
+import com.github.arteam.simplejsonrpc.core.domain.ErrorMessage;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Date: 8/9/14
+ * Time: 10:08 PM
+ * Represents JSON-RPC error returned by a server
+ *
+ * @author Artem Prigoda
+ */
+public class JsonRpcException extends RuntimeException {
+
+    /**
+     * Actual error message
+     */
+    @NotNull
+    private ErrorMessage errorMessage;
+
+    public JsonRpcException(@NotNull ErrorMessage errorMessage) {
+        super(errorMessage.toString());
+        this.errorMessage = errorMessage;
+    }
+
+    @NotNull
+    public ErrorMessage getErrorMessage() {
+        return errorMessage;
+    }
+}
