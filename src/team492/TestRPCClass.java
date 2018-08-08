156c1ebcb5c60ad8f21890e14fbde9b72c1e40f9
_c0da_ <duvictor514@gmail.com>
2018-08-07 20:26:20 -0700

    Implemented RPC service and class CORRECTLY

--- src/team492/TestRPCClass.java
+++ src/team492/TestRPCClass.java
@@ -5,29 +5,41 @@ import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class TestRPCClass 
+public class TestRPCClass
 {
-	@JsonProperty
-	private String firstName;
-	private String lastName;
-	private long timeInitialized = 0;
-	
-	@JsonCreator
-	public TestRPCClass(@JsonProperty("firstName") String firstName, @JsonProperty("lastName") String lastName)
-	{
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-	
-	public void initTime()
-	{
-		Date date = new Date();
-		timeInitialized = date.getTime();
-	}
-	
-	@Override
-	public String toString()
-	{
-		return firstName + " " + lastName + " " + timeInitialized;
-	}
+    @JsonProperty
+    private String firstName;
+
+    @JsonProperty
+    private String lastName;
+
+    @JsonProperty
+    private long timeInitialized = 0;
+
+
+    @JsonCreator
+    public TestRPCClass()
+    {
+        firstName = "Tail";
+        lastName = "Fuzzball";
+    }
+
+    @JsonCreator
+    public TestRPCClass(@JsonProperty("firstName") String firstName, @JsonProperty("lastName") String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void initTime()
+    {
+        Date date = new Date();
+        timeInitialized = date.getTime();
+    }
+
+    @Override
+    public String toString()
+    {
+        return firstName + " " + lastName + " " + timeInitialized;
+    }
 }
