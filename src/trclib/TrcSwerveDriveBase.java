26c25e7d2fc9fbe9fce6d8e4d7da72b21d455489
trc492 <trc492@live.com>
2018-08-25 02:21:18 -0700

    Merged Abhay's fix on rotation scale.

--- src/trclib/TrcSwerveDriveBase.java
+++ src/trclib/TrcSwerveDriveBase.java
@@ -105,16 +105,10 @@ public class TrcSwerveDriveBase extends TrcSimpleDriveBase
     @Override
     public void setPositionScales(double xScale, double yScale)
     {
-        double semiXAxis = wheelBaseWidth / Math.sqrt(2.0);
-        double semiYAxis = wheelBaseLength / Math.sqrt(2.0);
-
-        // The following black magic calculation is Ramanujan's method of approximating the perimeter of an ellipse.
-        double h = Math.pow(semiXAxis - semiYAxis, 2) / Math.pow(semiXAxis + semiYAxis, 2);
-        double perimeter = Math.PI * (semiXAxis + semiYAxis) * (1.0 + (3.0 * h / (10.0 + Math.sqrt(4.0 - 3.0 * h))));
-
         // encDist / perimeter = rotPos / 360.0
         // encDist * 360.0 / perimeter = rotPos
         // Therefore, rotScale = 360.0 / perimeter
+        double perimeter = wheelBaseDiagonal * Math.PI;
         double rotScale = 360.0 / perimeter;
 
         super.setPositionScales(xScale, yScale, rotScale);
