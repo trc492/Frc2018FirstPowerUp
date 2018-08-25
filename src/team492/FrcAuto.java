061e994954087820b6077bd16e2b2d6565ab3743
Abhay D <a.deshpande012@gmail.com>
2018-08-25 02:06:21 -0700

    Revert "More code cleanup"
    This reverts commit 88d524f31bec0831d4719ee17c94d0849ef84e7a.

--- src/team492/FrcAuto.java
+++ src/team492/FrcAuto.java
@@ -74,10 +74,22 @@ public class FrcAuto implements TrcRobot.RobotMode
     // Menus.
     //
     private FrcChoiceMenu<AutoStrategy> autoStrategyMenu;
+    private FrcChoiceMenu<RobotInfo.Position> startPositionMenu;
+    private FrcChoiceMenu<YesOrNo> fastDeliveryMenu;
+    private FrcChoiceMenu<YesOrNo> getSecondCubeMenu;
+    private FrcChoiceMenu<Lane> laneMenu;
+    private FrcChoiceMenu<ScaleOrSwitch> preferenceMenu;
+    private FrcChoiceMenu<YesOrNo> useSonarMenu;
 
     private AutoStrategy autoStrategy;
-
+    private RobotInfo.Position startPosition;
+    private boolean fastDelivery;
+    private boolean getSecondCube;
+    private Lane lane;
     private double delay;
+    
+    private double forwardDriveDistance;
+
     private TrcRobot.RobotCommand autoCommand;
     private MotionProfileTest test;
 
@@ -88,6 +100,12 @@ public class FrcAuto implements TrcRobot.RobotMode
         // Create Autonomous Mode specific menus.
         //
         autoStrategyMenu = new FrcChoiceMenu<>("Auto/AutoStrategies");
+        startPositionMenu = new FrcChoiceMenu<>("Auto/StartPos");
+        fastDeliveryMenu = new FrcChoiceMenu<>("Auto/FastDelivery");
+        getSecondCubeMenu = new FrcChoiceMenu<>("Auto/GetSecondCube");
+        laneMenu = new FrcChoiceMenu<>("Auto/Lanes");
+        preferenceMenu = new FrcChoiceMenu<>("Auto/ScaleOrSwitch");
+        useSonarMenu = new FrcChoiceMenu<>("Auto/UseSonar");
 
         //
         // Populate Autonomous Mode menus.
@@ -100,8 +118,29 @@ public class FrcAuto implements TrcRobot.RobotMode
         autoStrategyMenu.addChoice("Y Distance Drive", AutoStrategy.Y_DISTANCE_DRIVE, false, false);
         autoStrategyMenu.addChoice("Turn Degrees", AutoStrategy.TURN_DEGREES, false, false);
         autoStrategyMenu.addChoice("Do Nothing", AutoStrategy.DO_NOTHING, false, true);
-        
+
         test = new MotionProfileTest("MP", robot);
+
+        startPositionMenu.addChoice("Left Side Start", RobotInfo.Position.LEFT_POS, false, false);
+        startPositionMenu.addChoice("Middle Start", RobotInfo.Position.MID_POS, true, false);
+        startPositionMenu.addChoice("Right Side Start", RobotInfo.Position.RIGHT_POS, false, true);
+
+        fastDeliveryMenu.addChoice("Yes", YesOrNo.YES, true, false);
+        fastDeliveryMenu.addChoice("No", YesOrNo.NO, false, true);
+
+        getSecondCubeMenu.addChoice("Yes", YesOrNo.YES, true, false);
+        getSecondCubeMenu.addChoice("No", YesOrNo.NO, false, true);
+
+        laneMenu.addChoice("Lane 1", Lane.LANE1, false, false);
+        laneMenu.addChoice("Lane 2", Lane.LANE2, true, false);
+        laneMenu.addChoice("Lane 3", Lane.LANE3, false, false);
+        laneMenu.addChoice("Custom Distance", Lane.CUSTOM, false, true);
+        
+        preferenceMenu.addChoice("Switch", ScaleOrSwitch.SWITCH, true, false);
+        preferenceMenu.addChoice("Scale", ScaleOrSwitch.SCALE, false, true);
+        
+        useSonarMenu.addChoice("Yes", YesOrNo.YES, true, false);
+        useSonarMenu.addChoice("No", YesOrNo.NO, false, true);
     } // FrcAuto
 
     //
@@ -126,7 +165,36 @@ public class FrcAuto implements TrcRobot.RobotMode
         //
         autoStrategy = autoStrategyMenu.getCurrentChoiceObject();
 
+        startPosition = startPositionMenu.getCurrentChoiceObject();
+
+        fastDelivery = fastDeliveryMenu.getCurrentChoiceObject() == YesOrNo.YES;
+        getSecondCube = getSecondCubeMenu.getCurrentChoiceObject() == YesOrNo.YES;
+
+        // CodeReview: Why do it here again? You already initialized forwardDriveDistance below.
+        forwardDriveDistance = HalDashboard.getNumber("Auto/FwdDistance", RobotInfo.FWD_DISTANCE_3);
+
+        lane = laneMenu.getCurrentChoiceObject();
+        switch (lane)
+        {
+            case LANE1:
+                forwardDriveDistance = RobotInfo.FWD_DISTANCE_1;
+                break;
+
+            case LANE2:
+                forwardDriveDistance = RobotInfo.FWD_DISTANCE_2;
+                break;
+
+            case LANE3:
+                forwardDriveDistance = RobotInfo.FWD_DISTANCE_3;
+                break;
+
+            case CUSTOM:
+                forwardDriveDistance = HalDashboard.getNumber("Auto/FwdDistance", RobotInfo.FWD_DISTANCE_3);
+                break;
+        }
+
         delay = HalDashboard.getNumber("Auto/Delay", 0.0);
+        boolean useSonar = useSonarMenu.getCurrentChoiceObject() == YesOrNo.YES;
 
         switch (autoStrategy)
         {
